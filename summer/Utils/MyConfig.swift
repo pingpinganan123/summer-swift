2fe001eaee344ce92b6dab8bda6c9db9ab5fb7e8
黄荣君 <l1091462907@gmail.com>
2017-09-08 08:13:26 +0800

    将系统链接升级到https

--- summer/Utils/MyConfig.swift
+++ summer/Utils/MyConfig.swift
@@ -1,18 +1,11 @@
-//
-//  MyConfig.swift
-//  summer
-//
-//  Created by FangLin on 2/10/17.
-//  Copyright © 2017 FangLin. All rights reserved.
-//
-
 import UIKit
 
-let URL_APP_ROOT = "http://m.knowall.cn"//域名地址//内测版
+// 服务器地址
+let URL_APP_ROOT = "https://m.knowall.cn"
 
 let WELCOME_IMAGES_COUNT = 3
 
-//推送
+// 极光推送
 let appkey = "cc92570c841a688af5adc5b0"
 let channel = "Publish channel"
 let isProduction = true
